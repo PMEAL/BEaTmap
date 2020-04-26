4b93b25599e1ba068bc2697b490f5dbc68c213b4
Ellsworth <ellsworth.bell@gmail.com>
2020-04-26 10:50:00 -0400

    unused functions removed, bet combo plot function outputs numpy mask, check functions outputs arrays

--- beatmap/vis/__init__.py
+++ beatmap/vis/__init__.py
@@ -1,7 +1,5 @@
 from ._figures import experimental_data_plot
-from ._figures import experimental_data_1stderiv_plot
 from ._figures import ssa_heatmap
-from ._figures import theta_heatmap
 from ._figures import err_heatmap
 from ._figures import diff_heatmap
 from ._figures import bet_combo_plot
