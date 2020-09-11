83b9aa70cd25f041b9ab924c1c661d297d8608e6
Amin Sadeghi <amin.sadeghi@live.com>
2020-09-11 13:34:48 -0400

    Set default logging level to WARNING

--- beatmap/__init__.py
+++ beatmap/__init__.py
@@ -1,7 +1,10 @@
-__version__ = "0.0.1"
+import logging
+logging.basicConfig(level=logging.WARNING)
 
 from . import utils
 from . import vis
 from . import io
 from . import core
 from .core import run_beatmap
+
+__version__ = "0.0.1"
