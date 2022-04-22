fbc0a360f9cc901c4dc11264becec3a2c8614600
Amin Sadeghi <amin.sadeghi@live.com>
2022-04-21 22:47:11 -0400

    Fixed setup.py to include README.md in pypi

--- setup.py
+++ setup.py
@@ -14,10 +14,16 @@ with open(ver_path) as f:
         if line.startswith("__version__"):
             exec(line, main)
 
+# Read the contents of README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="beatmap",
     description="A tool for determining the valid P/P0 range in BET isotherms",
     zip_safe=False,
+    long_description=long_description,
     long_description_content_type='text/markdown',
     version=main["__version__"],
     classifiers=[
