57c366f11443fef2d3c84cdaeb548f271cec8d77
Amin Sadeghi <amin.sadeghi@live.com>
2022-04-21 21:36:26 -0400

    Minor refactoring of docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+from datetime import datetime
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
@@ -20,8 +21,8 @@ sys.path.insert(0, os.path.abspath('../../../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'beatmap'
-copyright = '2022, PMEAL Team'
+project = 'BEaTmap'
+copyright = f'{datetime.now().year}, PMEAL'
 author = 'PMEAL Team'
 
 # The full version, including alpha/beta/rc tags
@@ -63,4 +64,5 @@ html_theme = 'furo'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
+html_css_files = ['custom.css']
