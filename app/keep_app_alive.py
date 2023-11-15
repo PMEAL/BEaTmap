0153160485be467d560991af03a3a87226ebecb4
Amin Sadeghi <amin.sadeghi@live.com>
2023-11-15 05:52:35 -0500

    Selenium script to nudge streamlit app no longer needed

--- app/keep_app_alive.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-
-with webdriver.Chrome(options=options) as driver:
-    driver.get("https://beatmap.streamlit.app/")
